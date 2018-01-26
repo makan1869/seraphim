27d492c4753f06e5e5e1f52dcbb869b234a878be
Makan Sepehri <makan1869@gmail.com>
2018-01-26 16:51:29 +0330

    first commit

---
+++ src/main/java/ir/serenade/seraphim/SeraphimApplication.java
@@ -0,0 +1,12 @@
+package ir.serenade.seraphim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeraphimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeraphimApplication.class, args);
+	}
+}
